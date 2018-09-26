794ce22cb9a9672bc497f068b3e528b41ac0f60d
ramzi tannous <ramzi271996@gmail.com>
2018-09-26 16:35:46 +0300

    ff

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, request, flash, redirect, Response
+from werkzeug.utils import secure_filename
 
 from constants import DBTYPES
-from train import Trainer
+from train import Trainer, os
 
 app = Flask(__name__)
 app.secret_key = "841578451845asdasdasfasf"
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-# app.config['UPLOAD_FOLDER'] = Paths.UPLOAD_FOLDER
 
 whiteListTrainer = Trainer(DBTYPES.WHITELIST)
 blackListTrainer = Trainer(DBTYPES.BLACKLIST)
@@ -14,11 +14,10 @@ blackListTrainer = Trainer(DBTYPES.BLACKLIST)
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS \
- \
-           @ app.route("/names/<dbType>")
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+@app.route("/<dbType>/names")
 def getNames(dbType):
     if dbType == DBTYPES.WHITELIST:
         return whiteListTrainer.readNames()
@@ -26,39 +25,87 @@ def getNames(dbType):
         return blackListTrainer.readNames()
 
 
-# @app.route("/images/<name>",methods=['POST'])
-# def upload_file(name):
-#     path=os.path.join(app.config["UPLOAD_FOLDER"],name)
-#     if not os.path.exists(path):
-#         os.mkdir(path)
-#     if request.method == 'POST':
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename) and os.path.exists(path):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(path,filename))
-#             return Response(status=200)
-#         else:
-#             return Response(status=404)
-#
-# @app.route("/name",methods=['POST'])
-# def addUser():
-#     if request.method=="POST":
-#         try:
-#             name=request.json["name"]
-#             os.mkdir(Paths.UPLOAD_FOLDER+"/"+name)
-#             names=trainer.readNames()
-#             names.append(name)
-#             saved_names = np.array(names)
-#             np.savetxt(Paths.KNOWN_NAME_FILE, saved_names, delimiter=",", fmt="%s")
-#             return Response(status=200)
-#         except :
-#             return Response(status=500)
+@app.route("/<dbType>/images", methods=['POST'])
+def upload_file(dbType):
+    name = request.json["name"]
+    if dbType == DBTYPES.WHITELIST:
+        path = whiteListTrainer.imagesPath % name
+    else:
+        blackListTrainer.imagesPath % name
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename) and os.path.exists(path):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(path, filename))
+            return Response(status=200)
+        else:
+            return Response(status=404)
+
+
+@app.route("/<dbType>/add_name", methods=['POST'])
+def addUser(dbType):
+    if request.method == "POST":
+        try:
+            name = request.json["name"]
+            if dbType == DBTYPES.WHITELIST:
+                whiteListTrainer.addName(name)
+            else:
+                blackListTrainer.addName(name)
+            return Response(status=200)
+        except:
+            return Response(status=500)
+
+
+@app.route("/<dbType>/remove_name", methods=['POST'])
+def addUser(dbType):
+    if request.method == "POST":
+        try:
+            name = request.json["name"]
+            if dbType == DBTYPES.WHITELIST:
+                whiteListTrainer.removeName(name)
+            else:
+                blackListTrainer.removeName(name)
+            return Response(status=200)
+        except:
+            return Response(status=500)
+
+
+@app.route("/<dbType>/train", methods=['POST'])
+def addUser(dbType):
+    if request.method == "POST":
+        try:
+            name = request.json["name"]
+            if dbType == DBTYPES.WHITELIST:
+                whiteListTrainer.trainPerson(name)
+            else:
+                blackListTrainer.trainPerson(name)
+            return Response(status=200)
+        except:
+            return Response(status=500)
+
+
+@app.route("/<dbType>/train_image", methods=['POST'])
+def addUser(dbType):
+    if request.method == "POST":
+        try:
+            name = request.json["name"]
+            image = request.json["image"]
+            if dbType == DBTYPES.WHITELIST:
+                whiteListTrainer.trainImage(name, image)
+            else:
+                blackListTrainer.trainImage(name, image)
+            return Response(status=200)
+        except:
+            return Response(status=500)
 
 
 if __name__ == '__main__':
